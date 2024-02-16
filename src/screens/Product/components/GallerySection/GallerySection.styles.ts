1da896dee13fa9eeabd5c1ebd1536e32d726855c
Ramon Trovão <ramonzinhg12@gmail.com>
2024-02-15 23:13:52 -0300

    refactor: add padding to gallery section container

--- src/screens/Product/components/GallerySection/GallerySection.styles.ts
+++ src/screens/Product/components/GallerySection/GallerySection.styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components/native";
 
 export const GallerySectionContainer = styled.View`
-  padding: 0 ${({ theme }) => theme.SPACING.XL}px;
+  padding: ${({ theme }) => theme.SPACING.XL}px;
 `;
