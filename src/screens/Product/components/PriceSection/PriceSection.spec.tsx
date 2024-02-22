e51eb9e0e84952c0aacc3490c364bf204c990fa5
Ramon Trovão <ramonzinhg12@gmail.com>
2024-02-21 21:43:20 -0300

    test: add DescriptionSection & PriceSection unit tests

---
+++ src/screens/Product/components/PriceSection/PriceSection.spec.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@/utils/test-utils";
+import { PriceSection } from "./PriceSection";
+
+const makeSut = () => render(<PriceSection price={50} />)
+
+describe("Component - <PriceSection />", () => {
+  it("should render price", () => {
+    makeSut()
+    const priceText = screen.getByText("$ 50 / Year")
+
+    expect(priceText).toBeTruthy()
+  })
+})
